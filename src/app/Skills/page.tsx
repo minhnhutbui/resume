aa77a97741a5e5cbd08c733dd152c8bfd7600dab
minhnhutbui <rinrip225@gmail.com>
2024-08-31 13:21:03 +1000

    Initialise card component and skills page

--- src/app/Skills/page.tsx
+++ src/app/Skills/page.tsx
@@ -1,11 +1,18 @@
 // import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
+import Card from '@/components/Card';
 
 function Home() {
-  return (
-    <div className="light-theme-brown">
-      <NavBar />
-    </div>
-  );
+    return (
+        <div className="xl:max-w-[1400px] 2xl:max-w-screen-2xl w-full h-full mx-auto">
+            <div className="w-1/3">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    );
 }
 export default Home;
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,18 @@
+function Card() {
+    return (
+        <div className="w-full flex flex-col items-center">
+            <div className="w-1/3 flex justify-center mb-4">
+                <img src="/assets/img/html.png" alt="" />
+            </div>
+            <div className="mx-auto flex flex-col items-center justify-center text-justify">
+                <span className="block mb-4">HTML</span>
+                <span>
+                    Proficient in creating the structure and content of web pages, ensuring semantic and accessible
+                    code.
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
