c6306b468bccd178e681d4285fe145e9ec42c98c
minhnhutbui <rinrip225@gmail.com>
2024-09-01 12:03:33 +1000

    Add missing style for card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,10 @@
 function Card() {
     return (
-        <div className="w-full flex flex-col items-center">
+        <div className="w-full flex flex-col items-center p-10">
             <div className="w-1/3 flex justify-center mb-4">
                 <img src="/assets/img/html.png" alt="" />
             </div>
-            <div className="mx-auto flex flex-col items-center justify-center text-justify">
+            <div className="mx-auto flex flex-col items-center justify-center text-center">
                 <span className="block mb-4">HTML</span>
                 <span>
                     Proficient in creating the structure and content of web pages, ensuring semantic and accessible
